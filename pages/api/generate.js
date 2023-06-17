a14bf233e38bb1c30cd35d0fb0f9a89f0ddd587d
MetaNwar <90913195+MetaNwar@users.noreply.github.com>
2023-06-17 15:58:58 -0400

    GTFOL Build

---
+++ pages/api/generate.js
@@ -0,0 +1,30 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const basePromptPrefix = 
+`
+Write me an engaging twitter thread. Number each tweet. Include hashtags for each tweet. Include emojis. Include an elipses at the end of the first tweet. Please make sure the tweets go in-depth and show that the writer is an expert on the topic.
+`
+;
+const generateAction = async (req, res) => {
+  // Run first prompt
+  console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+
+  const baseCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${basePromptPrefix}${req.body.userInput}\n`,
+    temperature: 0.7,
+    max_tokens: 300,
+  });
+  
+  const basePromptOutput = baseCompletion.data.choices.pop();
+
+  res.status(200).json({ output: basePromptOutput });
+};
+
+export default generateAction;
